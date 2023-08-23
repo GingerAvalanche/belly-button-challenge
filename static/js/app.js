000eef4ef03373f9a818cfc139b986ffbb38e0ab
GingerAvalanche <chodness@gmail.com>
2023-08-23 09:24:44 -0700

    Populate dropdown

--- static/js/app.js
+++ static/js/app.js
@@ -2,4 +2,6 @@ let samples;
 
 d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then(j => {
     samples = j;
+    let dropdown = d3.select("#selDataset");
+    j.names.forEach(s => dropdown.insert("option").attr("value", s).text(s));
 });
